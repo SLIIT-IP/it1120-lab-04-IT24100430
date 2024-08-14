05c5ebf9baaf9fc52e21615a513038764d1b3422
IT24100430 <it24100430@my.sliit.lk>
2024-08-14 15:59:04 +0530

    Add files via upload

---
+++ Q1/IT24100430Lab4Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IT24100430Lab4Q1 {
+    public static void main(String[] arg) { 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+     }
+}
+        
